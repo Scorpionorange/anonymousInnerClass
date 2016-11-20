a8cbc7c86d29960625d49f546a5177d2fb8e8001
ScorpionOrange <scorpionorange@126.com>
2016-11-21 00:19:12 +0800

    Initial commit

---
+++ AnonymousInnerClassTest.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+/**
+ * Created by ScorpionOrange on 2016/08/09.
+ * update again in 2016.11.21.
+ */
+public class AnonymousInnerClassTest {
+    public static void main(String[] args){
+        TalkingClock clock = new TalkingClock();
+        clock.start(1000, true);
+
+        //keep program running until user selects "Ok"
+        JOptionPane.showMessageDialog(null, "Quit program ?");
+        System.exit(0);
+    }
+}
+
+/**
+ * A clock that prints the time in regular intervals.
+ */
+class TalkingClock{
+    public void start(int interval, final boolean beep){
+        ActionListener listener = new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                Date now = new Date();
+                System.out.println("At the moment, the time is " + now);
+                if(beep){
+                    Toolkit.getDefaultToolkit().beep();
+                }
+            }
+        };
+        Timer t = new Timer(interval, listener);
+        t.start();
+    }
+}
